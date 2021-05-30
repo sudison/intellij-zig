d5d5d9e1bb54a42e6ea47ffba1298619363b29a1
GitHub Action <action@github.com>
2021-05-30 11:48:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/sudison/ziglangide/MyBundle.kt
+++ src/main/kotlin/com/github/sudison/ziglangide/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sudison.ziglangide
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sudison/ziglangide/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sudison/ziglangide/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sudison.ziglangide.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sudison.ziglangide.MyBundle
 
 class MyApplicationService {
 
