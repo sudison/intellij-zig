b776eee1a3164cbc4660e2619007eb718148c670
Disheng Su <sudison@gmail.com>
2021-06-16 21:53:02 -0700

    ut works

--- src/main/kotlin/org/ziglang/ZigLangCompletionContributor.kt
+++ src/main/kotlin/org/ziglang/ZigLangCompletionContributor.kt
@@ -1,28 +1,26 @@
 package org.ziglang
 
 import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.AutoCompletionPolicy
+import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PatternCondition
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiErrorElement
-import com.intellij.psi.PsiWhiteSpace
 import com.intellij.util.ProcessingContext
 import org.ziglang.psi.ZigFile
 import org.ziglang.psi.ZigLangTypes
 
 
-class ZigCompletionProvider: CompletionProvider<CompletionParameters>() {
+class ZigCompletionProvider(private val les: List<LookupElement>) : CompletionProvider<CompletionParameters>() {
   override fun addCompletions(
     parameters: CompletionParameters,
     context: ProcessingContext,
     result: CompletionResultSet
   ) {
-
-    val builder = LookupElementBuilder.create(ZigLangTypes.PUB)
-    result.addElement(builder)
+    les.forEach(result::addElement)
   }
-
 }
 
 private class OnStatementBeginning(vararg startWords: String) : PatternCondition<PsiElement>("on statement beginning") {
@@ -31,14 +29,26 @@ private class OnStatementBeginning(vararg startWords: String) : PatternCondition
     return true
   }
 }
+
 class ZigLangCompletionContributor : CompletionContributor() {
 
-
-
+  private val topKeyWords = listOf(
+    ZigLangTypes.CONST,
+    ZigLangTypes.VAR,
+    ZigLangTypes.FN,
+    ZigLangTypes.PUB
+  ).map {
+    LookupElementBuilder
+      .create("$it ")
+      .withPresentableText(it.toString())
+      .withAutoCompletionPolicy(AutoCompletionPolicy.NEVER_AUTOCOMPLETE)
+  }
 
   init {
-    extend(CompletionType.BASIC,
-           psiElement().withParents(PsiErrorElement::class.java, ZigFile::class.java),
-           ZigCompletionProvider())
+    extend(
+      CompletionType.BASIC,
+      psiElement().withParents(PsiErrorElement::class.java, ZigFile::class.java),
+      ZigCompletionProvider(topKeyWords)
+    )
   }
 }
--- src/test/kotlin/org/ziglang/ZigLangCodeCompletionTest.kt
+++ src/test/kotlin/org/ziglang/ZigLangCodeCompletionTest.kt
@@ -1,17 +1,27 @@
 package org.ziglang
 
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixture4TestCase
-import org.junit.Assert
 import org.junit.Test
+import org.ziglang.psi.ZigLangTypes
 
 class ZigLangCodeCompletionTest: LightPlatformCodeInsightFixture4TestCase() {
   @Test
-  fun testA() {
+  fun testPub() {
     myFixture.configureByText(ZigFileType, "")
     myFixture.type("p")
 
-    Assert.assertEquals(1, myFixture.completeBasic().size)
+    val l = myFixture.completeBasic()
+    assertTrue(l.size == 1)
+    assertEquals(l[0].lookupString, "${ZigLangTypes.PUB} ")
   }
 
+  @Test
+  fun testFun() {
+    myFixture.configureByText(ZigFileType, "")
+    myFixture.type("pub f")
 
+    val l = myFixture.completeBasic()
+    assertTrue(l.size == 1)
+    assertEquals(l[0].lookupString, "${ZigLangTypes.FN} ")
+  }
 }
